f59a3bcabdaf361b22aa5d159aec7d09263e9d56
Alena Ligay <pandapo777@gmail.com>
2023-10-22 22:49:39 +0300

    second commit

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -18,16 +18,6 @@ test.each`
   expect(nameIsValid(a)).toBe(expected);
 });
 
-// test.each
-//   a    | op     | b    | expected
-//   ${2} | ${'*'} | ${2} | ${4}
-//   ${4} | ${'-'} | ${2} | ${2}
-//   ${2} | ${'+'} | ${2} | ${4}
-//   ${4} | ${'/'} | ${2} | ${2}
-// ('$a $op $p = $expect', ({a, op, b, expected}) => {
-//   expect(calculator(a, op, b)).toBe(expected);
-// });
-
 test.each`
   a                | expected
   ${"Alena"}       | ${"Alena"}
@@ -39,26 +29,10 @@ test.each`
   expect(fullTrim(a)).toBe(expected);
 });
 
-// test.each`
-//   a    | op     | b    | expected
-//   ${2} | ${'*'} | ${2} | ${4}
-//   ${4} | ${'-'} | ${2} | ${2}
-//   ${2} | ${'+'} | ${2} | ${4}
-//   ${4} | ${'/'} | ${2} | ${2}
-// `('$a $op $p = $expect', ({a, op, b, expected}) => {
-//   expect(getTotal(a, op, b)).toBe(expected);
-// });
-
 test.failing.each`
   discount | price | quantity | expected
   ${"0"}   | ${10} | ${10}    | ${"Скидка должна быть числом"}
   ${-15}   | ${10} | ${10}    | ${"Процент скидки не может быть отрицательным"}
-`("Checking getTotal", ({ discount, price, quantity, expected }) => {
-  expect(getTotal(discount, price, quantity)).toBe(expected);
-});
-
-test.each`
-  discount | price | quantity | expected
   ${0}     | ${10} | ${10}    | ${100}
   ${0}     | ${10} | ${1}     | ${10}
 `("Checking getTotal", ({ discount, price, quantity, expected }) => {
