ae5a246a1228afc52ccccaaa1459671352d2c8a2
Alena Ligay <pandapo777@gmail.com>
2023-10-25 13:17:41 +0300

    HomeworkUpgrade

--- specs/app.spec.js
+++ specs/app.spec.js
@@ -29,12 +29,40 @@ test.each`
   expect(fullTrim(a)).toBe(expected);
 });
 
-test.failing.each`
-  discount | price | quantity | expected
-  ${"0"}   | ${10} | ${10}    | ${"Скидка должна быть числом"}
-  ${-15}   | ${10} | ${10}    | ${"Процент скидки не может быть отрицательным"}
-  ${0}     | ${10} | ${10}    | ${100}
-  ${0}     | ${10} | ${1}     | ${10}
-`("Checking getTotal", ({ discount, price, quantity, expected }) => {
-  expect(getTotal(discount, price, quantity)).toBe(expected);
+test("checking getTotal, ошибка (Скидка должна быть числом)", () => {
+  expect(() => {
+    getTotal([], "number");
+  }).toThrow("Скидка должна быть числом");
 });
+
+test("checking getTotal2, ошибка (Процент скидки не может быть отрицательным)", () => {
+  expect(() => {
+    getTotal([], -1);
+  }).toThrow("Процент скидки не может быть отрицательным");
+});
+
+test.each([
+  { parameters: [[{ price: 10, quantity: 10 }], 0], expected: 100 },
+  { parameters: [[{ price: 10, quantity: 1 }, { price: 10, quantity: 9 }], 10], expected: 90 },
+  { parameters: [[{ price: 10, quantity: 1 }, { price: 10, quantity: 9 }], 1], expected: 99},
+])("Checking getTotal", ({ parameters, expected }) => {
+expect(getTotal(...parameters)).toBe(expected)
+})
+
+// test.each([
+//   { parameters: [[{ price: 10, quantity: 1 }, { price: 10, quantity: 9 }], -1], expected: new Error("Процент скидки не может быть отрицательным")},
+
+
+// ])("Checking getTotal", ({ parameters, expected }) => {
+// expect(getTotal(...parameters)).toThrow()
+// })
+
+// test.failing.each`
+//   discount | price | quantity | expected
+//   ${"0"}   | ${10} | ${10}    | ${"Скидка должна быть числом"}
+//   ${-15}   | ${10} | ${10}    | ${"Процент скидки не может быть отрицательным"}
+//   ${0}     | ${10} | ${10}    | ${100}
+//   ${0}     | ${10} | ${1}     | ${10}
+// `("Checking getTotal", ({ discount, price, quantity, expected }) => {
+//   expect(getTotal(discount, price, quantity)).toBe(expected);
+// });
